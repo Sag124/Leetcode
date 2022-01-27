238e662e5843af404689b07becf9ccba2d80255e
Sag124 <sagar05091997@gmail.com>
2022-01-27 08:57:44 +0530

    Time: 9 ms (54.27%), Space: 67.6 MB (15.07%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Boolean> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], true);
-        }
-        
-        for (int i = 1; i <= nums.length; i++) {
-            if (!map.containsKey(i)) {
-                ans.add(i);
+            int new_index = Math.abs(nums[i]) - 1;
+            if (nums[new_index] > 0) {
+                nums[new_index] *= -1;
             }
         }
-        return ans;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (nums[i-1] > 0) {
+                res.add(i);
+            }
+        }
+        return res;
     }
 }
