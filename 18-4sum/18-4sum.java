62c0573367b1096acacf89f893f62ac96dfdc0c6
Sag124 <sagar05091997@gmail.com>
2022-02-03 20:53:48 -0700

    Time: 39 ms (36.54%), Space: 44.6 MB (25.86%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,54 +1,31 @@
 class Solution {
-    
-    public void createRes(List<List<Integer>> res, List<List<Integer>> smallAns, int val) {
-        for (List<Integer> sa : smallAns) {
-            sa.add(val);
-            res.add(sa);
-        }
-    }
-    
-    public List<List<Integer>> twoSum(int[] nums, int target, int si, int ei) {
-        List<List<Integer>> res = new ArrayList<>();
-        while (si < ei) {
-            int sum = nums[si] + nums[ei];
-            if (sum > target) {
-                ei--;
-            } else if (sum < target) {
-                si++;
-            } else {
-                ArrayList<Integer> sol = new ArrayList<>();
-                sol.add(nums[si]);
-                sol.add(nums[ei]);
-                res.add(sol);
-                si++;
-                ei--;
-                while (si < ei && nums[si] == nums[si-1]) si++;
-                while (si < ei && nums[ei] == nums[ei + 1]) ei--;
-            }
-        }
-        return res;
-    }
-    
-    public List<List<Integer>> threeSum(int[] nums, int target, int si, int ei) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = si; i <= ei; i++) {
-            if (i != si && nums[i] == nums[i-1]) continue;
-            List<List<Integer>> smallAns = twoSum(nums, target - nums[i], i+1, ei);
-            createRes(res, smallAns, nums[i]);
-        }
-        return res;
-    }
-    
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int si = 0;
-        int ei = nums.length - 1;
-        for (int i = si; i <= ei; i++) {
-            if (i != si && nums[i] == nums[i-1]) continue;
-            List<List<Integer>> smallAns = threeSum(nums, target - nums[i], i+1, ei);
-            createRes(res, smallAns, nums[i]);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j != i + 1 && nums[j] == nums[j-1]) continue;             
+                int si = j + 1;
+                int ei = nums.length - 1;
+                
+                while (si < ei) {
+                    
+                    int sum = nums[i] + nums[j] + nums[si] + nums[ei];
+                    if (sum > target) ei--;
+                    else if (sum < target) si++;
+                    else {
+                        res.add(new ArrayList(Arrays.asList(nums[i], nums[j], nums[si], nums[ei])));
+                        si++;
+                        ei--;
+                        while (si < ei && nums[si] == nums[si-1]) si++;
+                        while (si < ei && nums[ei] == nums[ei+1]) ei--;
+                    }
+                }
+            }
         }
+        
         return res;
     }
 }
