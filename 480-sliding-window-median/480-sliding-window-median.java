4064df6854e18d434f43e26cfcdf152c0b654cfa
Sag124 <sagar05091997@gmail.com>
2022-02-09 23:49:22 -0700

    Time: 123 ms (35.96%), Space: 54.4 MB (18.07%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.java
+++ 480-sliding-window-median/480-sliding-window-median.java
@@ -1,56 +1,65 @@
 class Solution {
-    Queue<Integer> minHeap;
-    Queue<Integer> maxHeap;
-    public Solution(){
+     Queue<Integer> minHeap;
+     Queue<Integer> maxHeap;
+        
+    public Solution() {
         minHeap = new PriorityQueue<>();
         maxHeap = new PriorityQueue<>(Collections.reverseOrder());
     }
-    private void add(int num){
-        //add element to the correct heap
-        if(maxHeap.isEmpty() || maxHeap.peek() > num){
+    
+    private void add(int num) {
+        if (maxHeap.size() == 0 || maxHeap.peek() > num) {
             maxHeap.add(num);
-        }else{
+        } else {
             minHeap.add(num);
         }
-        //balance heap
-        balance();
-    }
-    private void balance(){
-        if(maxHeap.size() > minHeap.size() + 1){
+        
+        if (maxHeap.size() > minHeap.size() + 1) {
             minHeap.add(maxHeap.poll());
-        }else if(minHeap.size() > maxHeap.size()){
+        } else if (minHeap.size() > maxHeap.size()) {
             maxHeap.add(minHeap.poll());
         }
     }
-    private void remove(int num){
-        //remove element from the correct heap
-        if(num <= maxHeap.peek()){
+    
+    private void remove(int num) {
+        if (num <= maxHeap.peek()) {
             maxHeap.remove(num);
-        }else{
+        } else {
             minHeap.remove(num);
         }
-        //balance heap
-        balance();
-    }
-    private double findMedian(){
-        if(maxHeap.size() > minHeap.size()) return maxHeap.peek();
-        if(minHeap.size() > maxHeap.size()) return minHeap.peek();
-        return maxHeap.peek()/2.0 + minHeap.peek() /2.0;
-    }
-    public double[] medianSlidingWindow(int[] nums, int k) {
-        int start = 0;
-        double[] result = new double[nums.length - k + 1 ];
         
-        for(int end=0; end < nums.length; end++) {
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.add(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+    
+    private double findMedian() {
+        double val = 0.0;
+        if (maxHeap.size() == minHeap.size()) {
+            val = maxHeap.peek() / 2.0 + minHeap.peek() / 2.0;
+        } else if (maxHeap.size() > minHeap.size()) {
+            val = maxHeap.peek();
+        } else if (maxHeap.size() < minHeap.size()) {
+            val = minHeap.peek();
+        }
+         
+        return val;
+    }
+    
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] result = new double[nums.length - k + 1];
+        int start = 0;
+        for (int end = 0; end < nums.length; end++) {
             add(nums[end]);
             int size = (end - start + 1);
-            if(size == k) {
+            if (size == k) {
                 result[start] = findMedian();
-                remove(nums[start]); // remove the start number as we move forward the next window size
-                start++; // move start window ahead
+                remove(nums[start]);
+                start++;
             }
         }
-        
         
         return result;
     }
