2a8bebf02caf711eb1281f46c2d437b793f1f689
Sag124 <sagar05091997@gmail.com>
2022-02-25 13:11:05 -0700

    Time: 1 ms (91.95%), Space: 42.9 MB (6.19%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,31 +1,52 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         
-        String[] words = s.split(" ");
-        if (pattern.length() != words.length) {
-            return false;
-        }
-        HashMap<Character, String> map = new HashMap<>();
-        HashMap<String, Boolean> used = new HashMap<>();
+//         String[] words = s.split(" ");
+//         if (pattern.length() != words.length) {
+//             return false;
+//         }
+//         HashMap<Character, String> map = new HashMap<>();
+//         HashMap<String, Boolean> used = new HashMap<>();
         
-        for (int i = 0; i < pattern.length(); i++) { 
+//         for (int i = 0; i < pattern.length(); i++) { 
+//             char ch = pattern.charAt(i);
+//             if (!map.containsKey(ch)) {
+//                 if (used.containsKey(words[i])) {
+//                     return false;
+//                 } else {
+//                     used.put(words[i], true);
+//                     map.put(ch, words[i]);
+//                 }
+//             } else {
+//                 // 
+//                 String res = map.get(ch);
+//                 if (!res.equals(words[i])) {
+//                     return false;
+//                 } 
+//             } 
+            
+//         }
+//         return true;
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) return false;
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < pattern.length(); i++) {
             char ch = pattern.charAt(i);
             if (!map.containsKey(ch)) {
-                if (used.containsKey(words[i])) {
-                    return false;
-                } else {
-                    used.put(words[i], true);
+                // check in hashset
+                if (!set.contains(words[i])) {
                     map.put(ch, words[i]);
+                    set.add(words[i]);
+                } else {
+                    return false;
                 }
             } else {
-                // 
-                String res = map.get(ch);
-                if (!res.equals(words[i])) {
-                    return false;
-                } 
-            } 
-            
+                if (!map.get(ch).equals(words[i])) return false;
+            }
         }
+        
         return true;
+        
     }
 }
